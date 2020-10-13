30fc540ba7d436720a5597a01c55783c5ced53d8
Depcon print server <depcon@erste.hr>
2020-10-13 22:07:05 +0200

    latest

---
+++ ver2/mAluTend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls:  ['./assets/bootstrap/css/bootstrap.min.css']
+})
+export class AppComponent {
+  title = 'mAluTend';
+}
